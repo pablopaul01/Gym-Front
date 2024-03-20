81ea037da6793fe4729ece05034fe82cd28038e6
pablopaul01 <juanpablosalomon@gmail.com>
2024-03-19 23:56:50 -0300

    creando el login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import Login from '../components/login/Login'
-import "../components/login/login.css"
+import React, {useState} from 'react'
+import CardUser from '../components/login/CardUser'
+import LoginForm from '../components/login/LoginForm'
 
-const LoginPage = ({setIsLogged}) => {
-    return (
-        <div className="container-fluid shadow login">
-            <div className="row justify-content-center row-login">
-                <div className="col-lg-4 col-md-7 col-sm-9 col-xs-10 px-5 shadow div-login">
-                    <div className="py-5 titulo border border-top-0 border-end-0 border-start-0 d-flex justify-content-center align-items-center">
-                        <div className='col-6 d-flex justify-content-center align-items-center'>
-                            <h2 className="titulo-login text-center mb-0">Iniciar Sesión</h2>
-                        </div>
-                        <div className='d-none d-md-block col-6 text-center'>
-                            <img src='https://res.cloudinary.com/dtkrptodh/image/upload/v1707952355/media/1-PS_xrqile.png' className="img-fluid" alt="logo" style={{width:"70%"}}/>
-                        </div>
-                    </div>
-                    <Login setIsLogged={setIsLogged}/>
-                </div>
-            </div>
-        </div>
-    )
+const Login = ({setIsLogged}) => {
+
+
+  return (
+    <div className='min-h-[800px] h-[90vh] bg-slate-100 flex justify-center items-center'>
+        <CardUser title={"Iniciar Sesión"} subTitle={"Ingrese su correo para iniciar sesión"}>
+            <LoginForm setIsLogged={setIsLogged}/>
+        </CardUser>
+    </div>
+  )
 }
 
-export default LoginPage
+export default Login
