0500e4ab8a98f38dedae559706632c4e879684fb
pablopaul01 <juanpablosalomon@gmail.com>
2024-03-20 10:40:45 -0300

    haciendo el login

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Login from './pages/Login'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
-
+  const [isLogged, setIsLogged] = useState(()=>{
+    return !!localStorage.getItem('token') || false
+  })
   return (
     <>
       <Toaster richColors/>
       <Routes>
-        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Login setIsLogged={setIsLogged}/>}/>
       </Routes>
     </>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CardUser from '../components/CardUser'
+import LoginForm from '../components/LoginForm'
 
-const Login = () => {
+const Login = ({setIsLogged}) => {
   return (
-    <div>Login</div>
+    <div className='min-h-[800px] h-[90vh] bg-slate-100 flex justify-center items-center'>
+        <CardUser title={"Iniciar Sesión"} subTitle={"Ingrese su correo para iniciar sesión"}>
+            <LoginForm setIsLogged={setIsLogged}/>
+        </CardUser>
+    </div>
   )
 }
 
